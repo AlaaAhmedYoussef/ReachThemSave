3fcbf76a32ddc4906818d4ce9fe7583b12032d76
Alaa Ahmed Youssef <master_here100@yahoo.com>
2020-05-09 07:17:14 +0200

    Set Basic Movement

---
+++ Assets/Rocket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputProcess();
+    }
+
+    private void InputProcess()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.left);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.right);
+        }
+    }
+}
